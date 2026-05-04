6b788b3e0bd45e07c3e36b59656a51c6e4165ba9
safonanayaw <safoasare17@gmail.com>
2026-05-04 17:44:00 +0100

    Update dependencies and Next config

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,14 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
         hostname: "pavodahdb.s3.us-east-1.amazonaws.com",
         pathname: "/**",
-      }, 
+      },
       {
         protocol: "https",
         hostname: "i.pinimg.com",
