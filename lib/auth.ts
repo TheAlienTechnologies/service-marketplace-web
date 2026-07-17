cc694db663d975d2e8750c641b62c1776cf30659
safonanayaw <safoasare17@gmail.com>
2026-07-17 16:59:51 +0100

    BETTER AUTH production domain correction

--- lib/auth.ts
+++ lib/auth.ts
@@ -16,8 +16,9 @@ export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3001",
   trustedOrigins: [
     "http://localhost:3001",
-    "http://localhost:3000", // Backend URL
-  ],
+    "http://localhost:3000",
+    process.env.BETTER_AUTH_URL ||  "http://localhost:3001" // Backend URL
+  ].filter(Boolean) as string[],
 })
 
 export type Session = typeof auth.$Infer.Session
